10b5684317cd6bbbd1b540ddf3303ca48c82ff19
Sebastien Foubert <sebastien.foubert@gmail.com>
2018-06-28 23:30:42 +0200

    add content

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -65,7 +65,7 @@ class Gallery extends Component {
 
         const gallery = images.map((obj, i) => {
             return (
-                <article className="3u 12u$(xsmall) work-item" key={i}>
+                <article className="4u 12u$(xsmall) work-item" key={i}>
                     <a
                         className="image fit thumb"
                         href={obj.src}
