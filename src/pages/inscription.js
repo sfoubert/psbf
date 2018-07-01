83555b66214dea52e2c0c78d415e65ca869dc67e
Sebastien Foubert <sebastien.foubert@gmail.com>
2018-07-01 23:42:21 +0200

    fix: description meta

--- src/pages/inscription.js
+++ src/pages/inscription.js
@@ -30,7 +30,7 @@ class Inscription extends React.Component {
                 <Helmet title={'Inscription | ' + get(this, 'props.data.site.siteMetadata.title')}>
                     <meta name="description" content="Club de Savate Boxe Française sur Paris"/>
                     <meta name="robots" content="index,follow" />
-                    <link rel="canonical" href={ get(this, 'props.data.site.siteMetadata.siteUrl') } />
+                    <link rel="canonical" href="http://www.parissavate.fr" />
                 </Helmet>
 
                 <Waypoint
