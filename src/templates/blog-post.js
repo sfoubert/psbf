9acab267ec145706aa6c27d4bf793d88e2c8eb52
Sebastien Foubert <Sebastien.Foubert@ext.saint-gobain.com>
2018-07-20 16:06:13 +0200

    fix: blog posts layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,37 +2,110 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
+import Waypoint from 'react-waypoint'
+
+import Nav from '../components/Nav'
 
 class BlogPostTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stickyNav: false
+    }
+  }
+
+  _handleWaypointEnter = () => {
+    this.setState(() => ({stickyNav: false}));
+  };
+
+  _handleWaypointLeave = () => {
+    this.setState(() => ({stickyNav: true}));
+  };
+
   render() {
     const post = this.props.data.markdownRemark;
     const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+    const {previous, next} = this.props.pathContext;
 
     return (
-        <div>
-            <Helmet title={`${post.frontmatter.title} | ${siteTitle}`}/>
+      <div>
+        <Helmet title={`${post.frontmatter.title} | ${siteTitle}`}/>
+
+        <Waypoint
+          onEnter={this._handleWaypointEnter}
+          onLeave={this._handleWaypointLeave}
+        >
+        </Waypoint>
+        <Nav sticky={this.state.stickyNav}/>
+
+        <div id="main">
+          <section id="post" className="main">
+
+            <ul
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                listStyle: 'none',
+                padding: 0,
+              }}
+            >
+              <li>
+                {
+                  previous &&
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                }
+              </li>
+              <li>
+                {
+                  next &&
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                }
+              </li>
+            </ul>
+
             <h1>{post.frontmatter.title}</h1>
             <p>
-                {post.frontmatter.date}
+              {post.frontmatter.date}
             </p>
 
-            <div id="main">
-                <section id="post" className="main">
-                    <ul className="actions">
-                        <li><Link to="/" className="button">&lt;&nbsp;Retour</Link></li>
-                    </ul>
+            <div dangerouslySetInnerHTML={{__html: post.html}}/>
 
-                    <div dangerouslySetInnerHTML={{__html: post.html}}/>
+            <ul
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                listStyle: 'none',
+                padding: 0,
+              }}
+            >
+              <li>
+                {
+                  previous &&
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                }
+              </li>
+              <li>
+                {
+                  next &&
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                }
+              </li>
+            </ul>
 
-                    <footer className="major">
-                        <ul className="actions">
-                            <li><Link to="/" className="button">Retour</Link></li>
-                        </ul>
-                    </footer>
-                </section>
-            </div>
-
+          </section>
         </div>
+
+      </div>
     )
   }
 }
@@ -52,7 +125,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "DD MMMM YYYY", locale: "fr")
       }
     }
   }
